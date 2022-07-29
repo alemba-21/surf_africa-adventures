97bb14e6d3a4a4ed03c2e12745467f2d3ac5a0b5
alemba-21 <alembakiragu@gmail.com>
2022-07-29 07:34:31 +0300

    commiting changes

--- src/Links/Booking.js
+++ src/Links/Booking.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-
+import Form from '../components/Form';
 import HeroImg2 from '../components/HeroImg2';
 import Packages from '../components/Packages';
 
@@ -11,6 +11,7 @@ const Booking = () => {
       <Navbar />
       <HeroImg2 heading="Welcome" text="We feel honoured and pleased that you have liked our offers and we would love to serve you today. So, let us take you through on our Booking process."/>
       <Packages />
+      <Form />
     </div>
   )
 }
