e27afe721b20c04679eea51bf2d41b5d7540d1be
alemba-21 <alembakiragu@gmail.com>
2022-08-03 10:12:47 +0300

    routing contact path

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Adventures from "./Links/Adventures";
+import Booking from "./Links/Booking";
+import Home from "./Links/Home";
+import Contact from "./Links/Contact";
+
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Adventures" element={<Adventures />} />
+        <Route path="/Booking" element={<Booking />} />
+        
+    </Routes>
+  );
+}
+
+export default App;
