77def6b449ad192db84d53e0d96a1df5021bda49
alemba-21 <alembakiragu@gmail.com>
2022-08-03 10:14:52 +0300

    routing adventures path

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Adventures" element={<Adventures />} />
         
+        <Route path="/Booking" element={<Booking />} />
         <Route path="/Contact" element={<Contact />} />
     </Routes>
   );
