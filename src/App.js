54578be2bb47a50599b75b42b8539c130394638a
alemba-21 <alembakiragu@gmail.com>
2022-08-03 10:15:45 +0300

    routing home path

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Contact from "./Links/Contact";
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
         
+        <Route path="/Adventures" element={<Adventures />} />
         <Route path="/Booking" element={<Booking />} />
         <Route path="/Contact" element={<Contact />} />
     </Routes>
